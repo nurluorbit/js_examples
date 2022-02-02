bdf2a86ee25a0f412cea682675d784e17b8ae2c5
nurluorbit <57810104+nurluorbit@users.noreply.github.com>
2022-02-02 13:32:35 +0300

    Add files via upload

---
+++ func_loop_obj_array.js
@@ -0,0 +1,41 @@
+function ArrayPlus (inputArray){
+  return (Number(inputArray.join(''))+1).toString().split('').map(Number)
+}
+
+let Arraycim = ArrayPlus([1,0,3]);
+console.log(Arraycim)
+
+let info = [{
+  "name": "Jason",
+  "gender": "M",
+  "age": 27
+},
+{
+  "name": "Rosita",
+  "gender": "F",
+  "age": 23
+},
+{
+  "name": "Leo",
+  "gender": "M",
+  "age": 19
+}];
+
+function nameobjectier(array=[]) {
+  output = {}
+
+  for (i in array) {
+      if(!array[i].name) {
+          console.log(`There isn't any key called name in ${Number(i)+1} element.`)
+          continue
+      }
+
+      let data = Object.entries(array[i]).filter(a => !a.includes('name'))
+
+      output[array[i].name] = Object.fromEntries(data)
+  }
+
+  return output;
+}
+
+console.log(nameobjectier(info))
